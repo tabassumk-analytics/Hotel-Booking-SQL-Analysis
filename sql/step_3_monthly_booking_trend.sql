6b69318e95a3691a6c7c5dcf48480f26cd6b8601
Tabassum khan <tabassum.a.khan02@gmail.com>
2025-06-02 14:29:32 -0700

    Step 3: Added SQL and output CSV for monthly booking trend analysis

---
+++ sql/step_3_monthly_booking_trend.sql
@@ -0,0 +1,23 @@
+--Step 3: Monthly Booking Trend
+
+select
+arrival_date_month,
+arrival_date_year,
+count(*) as monthly_booking
+from hotel_bookings
+group by arrival_date_month, arrival_date_year
+order by arrival_date_year,
+	case
+	when arrival_date_month = 'January' then 1
+	when arrival_date_month = 'February' then 2
+	when arrival_date_month = 'March' then 3
+	when arrival_date_month = 'April' then 4
+	when arrival_date_month = 'May' then 5
+	when arrival_date_month = 'June' then 6
+	when arrival_date_month = 'July' then 7
+	when arrival_date_month = 'August' then 8
+	when arrival_date_month = 'September' then 9
+	when arrival_date_month = 'October' then 10
+	when arrival_date_month = 'November' then 11
+	when arrival_date_month = 'December' then 12
+	end
